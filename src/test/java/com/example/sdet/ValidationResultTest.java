d4dd2ccdc14ee070bcbb485e3dd5da91edec1e7c
Dipan <60612087+dipan99@users.noreply.github.com>
2026-02-16 18:51:43 -0600

    front end setup

--- src/test/java/com/example/sdet/ValidationResultTest.java
+++ src/test/java/com/example/sdet/ValidationResultTest.java
@@ -21,4 +21,32 @@ class ValidationResultTest {
         assertTrue(r.getErrors().contains("Password must not be null"));
     }
 
+    @Test
+    void doesNotHaveUpper_fails() {
+        ValidationResult r = validator.validate("abcdef1!");
+        assertFalse(r.isValid());
+        assertTrue(r.getErrors().contains("Password must include at least one uppercase letter"));
+    }
+
+    @Test
+    void doesNotHaveLower_fails() {
+        ValidationResult r = validator.validate("ABCDEF1!");
+        assertFalse(r.isValid());
+        assertTrue(r.getErrors().contains("Password must include at least one lowercase letter"));
+    }
+
+    @Test
+    void doesNotHaveNumbers_fails() {
+        ValidationResult r = validator.validate("ABCDEFG!");
+        assertFalse(r.isValid());
+        assertTrue(r.getErrors().contains("Password must include at least one digit"));
+    }
+
+    @Test
+    void doesNotHaveSpecialCharacters_fails() {
+        ValidationResult r = validator.validate("ABCDEFGH");
+        assertFalse(r.isValid());
+        assertTrue(r.getErrors().contains("Password must include at least one special character"));
+    }
+
 }
